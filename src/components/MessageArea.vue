0fe76fb63723b7723ba031331150f914e4087572
Seunghoon Ko <imskojs@gmail.com>
2018-12-06 09:36:52 +0900

    send and clear form

--- src/components/MessageArea.vue
+++ src/components/MessageArea.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="message">
     <textarea class="message-area" v-model="form.message"></textarea>
+    <div class="button-container">
+      <button @click="send()">보내기</button>
+    </div>
   </div>
 </template>
 
@@ -11,6 +14,19 @@ export default {
     return {
       form: this.$globalData.form
     };
+  },
+  methods: {
+    send() {
+      let sentMessages = this.$globalData.data.messages;
+      this.form.date = Date.now();
+      sentMessages.unshift(Object.assign({}, this.form));
+      this.clearForm(this.form);
+    },
+    clearForm(form) {
+      form.friendType = "";
+      form.message = "";
+      form.date = null;
+    }
   }
 };
 </script>
