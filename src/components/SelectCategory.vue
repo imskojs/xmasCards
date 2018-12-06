27ed58227cb19bf3d32a2e84189ec9643be46284
Seunghoon Ko <imskojs@gmail.com>
2018-12-06 09:16:08 +0900

    select category

---
+++ src/components/SelectCategory.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="category">
+    <select v-model="form.friendType">
+      <option disabled value>카테고리를 선택하세요</option>
+      <option value="colleage">동료</option>
+      <option value="friend">친구</option>
+      <option value="family">가족</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SelectCategory",
+  data() {
+    return {
+      form: this.$globalData.form
+    };
+  },
+  props: {
+    friendTypes: Array
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.prototype.$globalData = {
+
+  form: {
+    friendType: '',
+    message: ''
+  },
+
+  data: {
+    messages: []
+  }
+
+}
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
